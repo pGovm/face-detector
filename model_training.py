3245a2831100e93da1c64208ee9309ebeb3eaf35
pGovm <pgovindmenon07@gmail.com>
2025-01-14 01:11:32 -0500

    Changed the folder names

--- model_training.py
+++ model_training.py
@@ -1,10 +1,10 @@
 from imageai.Detection.Custom import DetectionModelTrainer
 
 trainer = DetectionModelTrainer()
-data_directory="C:/Users/17048/face_detector/DataSet_Main"
+data_directory="C:/Users/17048/face-detector"
 trainer.setModelTypeAsYOLOv3()
 trainer.setDataDirectory(data_directory)
-trainer.setTrainConfig(object_names_array=["Ye"], batch_size=6, num_experiments=349, train_from_pretrained_model="C:/Users/17048/face_detector/models/tiny-yolov3.pt")
+trainer.setTrainConfig(object_names_array=["Ye"], batch_size=6, num_experiments=349, train_from_pretrained_model="C:/Users/17048/face-detector/models/tiny-yolov3.pt")
 try:
     trainer.trainModel()
 except AssertionError as e:
